d854ccb1c418840ebec64ee25f06a10b35b235b5
Fernando Rios <feriosch98@gmail.com>
2021-09-21 19:20:13 -0500

    Vocabulary home refactor

--- src/app/vocabulary/vocabulary-routing.module.ts
+++ src/app/vocabulary/vocabulary-routing.module.ts
@@ -2,7 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
-import { VocabularyHomeComponent } from './vocabulary-home/vocabulary-home.component';
+import { VocabularyHomeComponent } from './home/vocabulary-home.component';
 import { AddWordComponent } from './add-word/add-word.component';
 import { WordDetailsComponent } from './word-details/word-details.component';
 import { NotFoundComponent } from '../shared/not-found/not-found.component';
--- src/app/vocabulary/vocabulary.module.ts
+++ src/app/vocabulary/vocabulary.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AgGridModule } from 'ag-grid-angular';
 
-import { VocabularyHomeComponent } from './vocabulary-home/vocabulary-home.component';
+import { VocabularyHomeComponent } from './home/vocabulary-home.component';
 import { WordTableComponent } from './home/word-table/word-table.component';
 import { CollectionCardComponent } from './home/collection-card/collection-card.component';
 import { AddWordFormComponent } from './add-word/add-word-form/add-word-form.component';
