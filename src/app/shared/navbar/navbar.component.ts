14fa53342e00f0b80388ef3fbed1d759e9e08c41
Fernando Rios <feriosch98@gmail.com>
2022-07-11 09:11:45 -0500

    Navbar Add Grammar Tab

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -3,15 +3,13 @@ import { Router } from '@angular/router';
 
 import { AuthService } from '../../auth/auth.service';
 
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   isAuthenticated() {
     return this.authService.isAuthenticated;
@@ -29,10 +27,21 @@ export class NavbarComponent implements OnInit {
     await this.router.navigateByUrl('/kanji');
   }
 
+  async onClickAddKanji() {
+    await this.router.navigateByUrl('/kanji/add');
+  }
+
+  async onClickGrammar() {
+    await this.router.navigateByUrl('/grammar');
+  }
+
+  async onClickAddClause() {
+    await this.router.navigateByUrl('/grammar/add');
+  }
+
   async onClickLogout() {
     await this.authService.logout();
   }
 
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
