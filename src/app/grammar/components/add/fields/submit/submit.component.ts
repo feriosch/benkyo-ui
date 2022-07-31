919a782ddbbdfced1363ba81e0fedaadded88f8f
Fernando Rios <feriosch98@gmail.com>
2022-07-31 18:52:57 -0500

    Add Clause - Level Field & Submit Fixes

--- src/app/grammar/components/add/fields/submit/submit.component.ts
+++ src/app/grammar/components/add/fields/submit/submit.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormGroupDirective } from '@angular/forms';
 
-import { AddClauseBody } from 'src/models/requests/grammar/add-clause.model';
-import { AddClauseValuesTransformerService } from 'src/app/grammar/services/add-values-transformer.service';
-
 @Component({
   selector: 'app-add-clause-form-submit-button',
   templateUrl: './submit.component.html',
@@ -12,19 +9,9 @@ import { AddClauseValuesTransformerService } from 'src/app/grammar/services/add-
 export class AddClauseFormSubmitButtonComponent implements OnInit {
   formGroup?: FormGroup;
 
-  constructor(
-    private rootFormGroup: FormGroupDirective,
-    private valueTransformerService: AddClauseValuesTransformerService
-  ) {}
+  constructor(private rootFormGroup: FormGroupDirective) {}
 
   ngOnInit(): void {
     this.formGroup = this.rootFormGroup.control as FormGroup;
-  }
-
-  onSubmit(): void {
-    const addClauseBody: AddClauseBody = this.valueTransformerService.transform(
-      this.formGroup!.value
-    );
-    console.log(addClauseBody);
   }
 }
--- src/app/grammar/components/add/form/form.component.ts
+++ src/app/grammar/components/add/form/form.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { AddClauseBody } from 'src/models/requests/grammar/add-clause.model';
 import { NotificationService } from 'src/app/shared/notification.service';
 import { GrammarService } from 'src/app/grammar/services/grammar.service';
+import { AddClauseValuesTransformerService } from 'src/app/grammar/services/add-values-transformer.service';
 
 @Component({
   selector: 'app-add-clause-form',
@@ -15,13 +17,14 @@ export class AddClauseFormComponent implements OnInit {
 
   constructor(
     private grammarService: GrammarService,
+    private valueTransformerService: AddClauseValuesTransformerService,
     private notificationService: NotificationService
   ) {
     this.addClauseForm = new FormGroup({
       title: new FormControl(null, [Validators.required]),
       hiragana: new FormControl(null),
       translation: new FormControl(null, [Validators.required]),
-      level: new FormControl(null, [Validators.required]),
+      level: new FormControl('intermediate', [Validators.required]),
       type: new FormGroup({
         adjective: new FormControl(false),
         adverb: new FormControl(false),
@@ -52,4 +55,11 @@ export class AddClauseFormComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    const addClauseBody: AddClauseBody = this.valueTransformerService.transform(
+      this.addClauseForm.value
+    );
+    console.log(addClauseBody);
+  }
 }
