b9ea1cc8167ed6f5cbf0e71bdb908ab7ccdb8dec
Fernando Rios <feriosch98@gmail.com>
2023-05-07 13:58:48 +0900

    Style/Form Refactor - Collection Add Name Field

--- src/app/collections/components/add/fields/name/name.component.ts
+++ src/app/collections/components/add/fields/name/name.component.ts
@@ -1,21 +1,45 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { FormService } from 'src/app/shared/services/form.service';
 
 @Component({
   selector: 'app-add-collection-form-name-field',
   templateUrl: './name.component.html',
-  styleUrls: ['./name.component.scss'],
 })
 export class AddCollectionFormNameFieldComponent implements OnInit {
   @Input()
-  formGroup?: FormGroup;
+  form!: FormGroup;
 
-  constructor() {}
+  constructor(private formService: FormService) {}
+
+  get isPrintingNameInvalid(): boolean {
+    const control = this.formService.getControl<FormControl>(
+      this.form,
+      'printingName'
+    );
+    return control.touched && control.invalid;
+  }
+
+  get isCollectionNameInvalid(): boolean {
+    const control = this.formService.getControl<FormControl>(
+      this.form,
+      'collectionName'
+    );
+    return control.touched && control.invalid;
+  }
+
+  get isGroupInvalid(): boolean {
+    const control = this.formService.getControl<FormControl>(
+      this.form,
+      'group'
+    );
+    return control.touched && control.invalid;
+  }
 
   ngOnInit(): void {}
 
   isControlInvalid(name: string): boolean {
-    const control: FormControl = this.formGroup!.get(name) as FormControl;
+    const control = this.formService.getControl<FormControl>(this.form, name);
     return control.touched && control.invalid;
   }
 }
