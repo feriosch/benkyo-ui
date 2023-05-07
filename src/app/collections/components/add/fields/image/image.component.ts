e606e373eda5c06e9e7202a5b94fbb025baf3a58
Fernando Rios <feriosch98@gmail.com>
2023-05-07 13:47:17 +0900

    Style/Form Refactor - Collection Add Image Field

--- src/app/collections/components/add/fields/image/image.component.ts
+++ src/app/collections/components/add/fields/image/image.component.ts
@@ -2,44 +2,47 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Form, FormControl, FormGroup } from '@angular/forms';
 
 import { FileTypeValidatorService } from 'src/app/collections/services/validators/file-type.service';
+import { FormService } from 'src/app/shared/services/form.service';
 
 @Component({
   selector: 'app-add-collection-form-image-field',
   templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss'],
 })
 export class AddCollectionFormImageFieldComponent implements OnInit {
   @Input()
-  formGroup?: FormGroup;
+  form!: FormGroup;
 
-  constructor(private fileTypeValidator: FileTypeValidatorService) {}
+  constructor(
+    private fileTypeValidator: FileTypeValidatorService,
+    private formService: FormService
+  ) {}
 
-  ngOnInit(): void {}
-
-  get validFileTypes(): string {
+  get fileTypes(): string {
     return this.fileTypeValidator.validTypes.toString();
   }
 
-  get imagePathControl(): FormControl {
-    return this.formGroup!.get('imagePath')! as FormControl;
+  get pathControl(): FormControl {
+    return this.formService.getControl<FormControl>(this.form, 'imagePath');
   }
 
-  get imageFileControl(): FormControl {
-    return this.formGroup!.get('imageFile')! as FormControl;
+  get fileControl(): FormControl {
+    return this.formService.getControl<FormControl>(this.form, 'imageFile');
   }
 
-  get isImagePathValid(): boolean {
-    return this.imagePathControl.touched && this.imagePathControl.valid;
+  get isPathValid(): boolean {
+    return this.pathControl.touched && this.pathControl.valid;
   }
 
-  get isImagePathInvalid(): boolean {
-    return this.imagePathControl.touched && this.imagePathControl.invalid;
+  get isPathInvalid(): boolean {
+    return this.pathControl.touched && this.pathControl.invalid;
   }
+
+  ngOnInit(): void {}
 
   uploadImage(event: Event) {
     const file = (event.target as HTMLInputElement).files![0];
-    this.imagePathControl.patchValue(file.name);
-    this.imageFileControl.patchValue(file);
-    this.imageFileControl.updateValueAndValidity();
+    this.pathControl.patchValue(file.name);
+    this.fileControl.patchValue(file);
+    this.fileControl.updateValueAndValidity();
   }
 }
